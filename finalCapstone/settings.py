8891241ab4ffe1b344ef2babe3c528fa958922eb
Seungmin Ham <hamsm3184@naver.com>
2024-10-15 19:42:26 +0900

    server setting

--- finalCapstone/settings.py
+++ finalCapstone/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-sck3$w3hl=lv=t&p*^95apyb4&b7is#vz2&b__8rm-m16_8&#-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["144.24.66.142"]
 
 # Application definition
 
