6f84e9cae4cd1537850aa4811bc38a20b61e4757
Kimbh0604 <kimbh980604@gmail.com>
2024-10-18 23:38:59 +0900

    차트 새로고침 구현

--- trading/urls.py
+++ trading/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('trading/', views.trading, name='trading'),
     path('place_order/', views.place_order, name='place_order'),
     path('place_order_sell', views.place_order_sell, name='place_order_sell'),
+    path('get_stock_data/', views.get_stock_data, name='get_stock_data'),
     path('articles/', include('articles.urls')),
 ]
