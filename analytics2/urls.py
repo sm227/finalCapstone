a2979c6d2d07763b3ab5696d7e8a0dec6d5c6208
Seungmin Ham <hamsm3184@naver.com>
2024-12-05 15:30:13 +0900

    Adjusting your investment allocation

--- analytics2/urls.py
+++ analytics2/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('stock-analysis/', views.stock_analysis, name='stock_analysis'),
     path('update-investment-style/', views.update_investment_style, name='update_investment_style'),
     path('update-auto-investment/', views.update_auto_investment, name='update_auto_investment'),
+    path('update-portfolio-settings/', views.update_portfolio_settings, name='update_portfolio_settings'),
 ]
